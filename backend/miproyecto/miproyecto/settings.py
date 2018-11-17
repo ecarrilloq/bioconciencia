0efa63fb3e3880c54370d69e5d8439fd37c71a3d
jvelasquezv <jdelrojo1927@gmail.com>
2018-11-17 14:16:39 -0500

    autenticacion

--- backend/miproyecto/miproyecto/settings.py
+++ backend/miproyecto/miproyecto/settings.py
@@ -126,3 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+     ),
+     'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', 
+    ), 
+}
